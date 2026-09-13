9a1013dfc36c6f936344b07cf0265bb8822a4022
Emanuel0933 <emanuelperea243@gamil.com>
2026-09-13 12:12:21 -0600

    modifique el login

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer class="footer">
-    <p>Sistema de Biblioteca &mdash; Proyecto académico PHP 8 + MySQL &copy; <?= date('Y') ?></p>
+    <p>Sistema de Biblioteca </p>
 </footer>
 </body>
 </html>
